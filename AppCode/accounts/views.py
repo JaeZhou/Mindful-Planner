60eef47d54f765fc18185f7b034aea9630184d26
ruddy-castro <60947941+ruddy-castro@users.noreply.github.com>
2021-08-28 15:17:15 -0700

    Registration Page Update
    Added custom fields to registration page.

--- AppCode/accounts/views.py
+++ AppCode/accounts/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
 # accounts/views.py
-from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import RegistrationForm 
 from django.urls import reverse_lazy
 from django.views import generic
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegistrationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
