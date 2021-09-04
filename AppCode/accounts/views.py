3ac91999c5c90f23b85d04d2228fe2ae1e5bced3
JaeZhou <marjoriebalaoro16@gmail.com>
2021-09-04 05:11:53 -0700

    uploading current work so i can work on my mac

--- AppCode/accounts/views.py
+++ AppCode/accounts/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 # accounts/views.py
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.urls.base import reverse
 from django.views import generic
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    template_name = 'signup.html'
+
+class ScheduleView(generic.CreateView):
+    success_url = reverse_lazy('schedule')
+    template_name = 'schedule.html'
