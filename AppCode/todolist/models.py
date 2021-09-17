9a212e9fef3f17ce6a0deb2aa2c259bd71d54266
nsutton98 <nsutton98@gmail.com>
2021-09-16 19:52:37 -0700

    Created to-do list
    Created models and views for task operations.
    Task adding is finished.
    Task editing and deleting still needs to be implemented.
    UI needs to be modified.

---
+++ AppCode/todolist/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    due_date = models.DateField(auto_now_add=False, blank=False)
+    due_time = models.TimeField(auto_now_add=False, blank=False)
+
+    def __str__(self):
+        return self.name
