2d7ed431d0fc0a796b6a8b8e57f9d9a8f45a6351
Fiona0131 <60491803+bobatea98@users.noreply.github.com>
2021-09-18 23:33:57 -0700

    Account settings (front+backend)
    1. Creating customers startapp folder (backend for account settings)
    2. Note: Still working on backend of background upload.

--- AppCode/MindfulPlanner/urls.py
+++ AppCode/MindfulPlanner/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+      path('customers/', include("customers.urls")),
     # path('', TemplateView.as_view(template_name='home.html'), name='home'),
 
     # The home page
