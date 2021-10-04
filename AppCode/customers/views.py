814dae072e3ae77249303fd63a2d735b7dda4ab3
Fiona0131 <60491803+bobatea98@users.noreply.github.com>
2021-10-03 22:17:03 -0700

    password changes update

--- AppCode/customers/views.py
+++ AppCode/customers/views.py
@@ -6,6 +6,10 @@ from django.views import View
 
 from customers.forms import ProfileForm, form_validation_error
 from customers.models import Profile
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.hashers import make_password
+
 
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
@@ -27,11 +31,23 @@ class ProfileView(View):
             profile = form.save()
             profile.user.first_name = form.cleaned_data.get('first_name')
             profile.user.last_name = form.cleaned_data.get('last_name')
-            profile.user.username = form.cleaned_data.get('username')
             profile.user.email = form.cleaned_data.get('email')
+
             profile.user.save()
+            password = form.cleaned_data.get('password')
+            username = form.cleaned_data.get('username')
+            if password != '******':
+                  password = form.cleaned_data.get('password')
+                  profile.user.password = make_password(password)
+                  profile.user.save()
+            if (password == '******') and (username != profile.user.username):
+                  profile.user.username = form.cleaned_data.get('username')
+                  profile.user.save()
+
+
 
             messages.success(request, 'Profile saved successfully')
         else:
             messages.error(request, form_validation_error(form))
         return redirect('profile')
+
