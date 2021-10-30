983e867e9a3b4574a58ef9a2ebe24e79c219d812
JaeZhou <38474976+JaeZhou@users.noreply.github.com>
2021-10-30 03:44:06 -0700

    changed timezone + added clock to dashboard

--- AppCode/MindfulPlanner/views.py
+++ AppCode/MindfulPlanner/views.py
@@ -7,17 +7,19 @@ from django.template import loader
 from django.http import HttpResponse
 from django import template
 
+
+# Loads Dashboard Page
 @login_required(login_url="/login/")
 def index(request):
     
     context = {}
     context['segment'] = 'index'
 
-    html_template = loader.get_template( 'index.html' )
+    html_template = loader.get_template( 'index.html')
     return HttpResponse(html_template.render(context, request))
 
+# Loads Landing Page
 def mainpage(request):
-    
     context = {}
     context['segment'] = 'index'
     html_template = loader.get_template( 'mainpage.html' )
